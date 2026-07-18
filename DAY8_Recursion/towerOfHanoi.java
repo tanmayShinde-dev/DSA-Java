fdf58a0f0f4e31a6fa5cd10b5372e00641e407dd
Tanmay shinde <tanmayshinde.dev@gmail.com>
2026-07-18 22:53:41 +0530

    Day 11: Recursion problems

---
+++ DAY8_Recursion/towerOfHanoi.java
@@ -0,0 +1,23 @@
+public class towerOfHanoi{
+
+    public static void towerOfHanoi( int n, String src, String helper, String dest){
+
+        if(n==1){
+            System.out.println("transfer disk " + n + " from " + src + " to " + dest);
+
+            return;
+        }
+        
+        towerOfHanoi(n-1, src, dest, helper);
+
+        System.out.println("transfer disk " + n + " from " + src + " to " + dest);
+
+        towerOfHanoi(n-1, helper, src, dest);
+    }
+    public static void main(String args[]){
+
+        int n = 3;
+        towerOfHanoi(n, "A", "B", "C");
+        
+    }
+}
